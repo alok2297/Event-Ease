af390086f74e591ad6bbf697021701e2e21fe2f1
Alok kumar <alokkumar2297812@gmail.com>
2023-06-30 01:26:26 +0530

    megre me adi

--- MainActivity.js
+++ MainActivity.js
@@ -4,6 +4,24 @@ var button = document.querySelector('.swiper-button-next');
 button.style.height = '30px';
 button.style.width = '10px';
 
+
+
+function toggleDropdown() {
+  var dropdownMenu = document.getElementById("hero_sec_dropdownMenu");
+  if (dropdownMenu.style.display === "none") {
+    dropdownMenu.style.display = "block";
+  } else {
+    dropdownMenu.style.display = "none";
+  }
+}
+
+function selectItem(item) {
+  var searchInput = document.getElementById("hero_sec_searchInput");
+  searchInput.value = item;
+  toggleDropdown();
+}
+
+
 var outerSlider = new Swiper(".card_slider", {
   slidesPerView: 3,
   spaceBetween: 30,
